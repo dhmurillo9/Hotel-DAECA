4bbec13b68269c022e404d5648017c7213e1ab6c
eespitia16 <eespitiapicon@gmail.com>
2018-09-17 07:28:42 -0500

    creacion del primer formulario

---
+++ js/main.js
@@ -0,0 +1,64 @@
+
+  function registrar(){
+    var nombre, apellidos, correo, usuario, clave, telefono, expresion;
+    nombre = document.getElementById('nombre').value;
+    apellidos = document.getElementById('apellidos').value;
+    correo = document.getElementById('correo').value;
+    usuario = document.getElementById('usuario').value;
+    clave = document.getElementById('clave').value;
+    telefono = document.getElementById('telefono').value;
+
+    if (nombre ===) {
+      alert("El campo nombre esta vacío");
+    }
+  }
+    
+    firebase.auth().createUserWithEmailAndPassword(nombre, apellidos, correo, clave, telefono,expresion)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+
+      // ...
+    });
+
+
+
+
+
+    const form = document.getElementById('contactForm'); // Obtenemos la referencia al formulario
+
+    if(form){ // Si existe nuestro elemento en memoria este se quedara escuchando al evento submit del formulario
+      form.addEventListener('submit', contactForm); // Al momento de enviar el formulario, ejecuta la función "contactform"
+    }
+
+    function contactForm(event) {
+      event.preventDefault(); // Prevenimos el comportamiento por defecto de un formulario (Enviar por URL los parametros)
+      const nombre = document.getElementById('nombre'); // Obtenemos la referencia a cada uno de nuestros elementos del formulario
+      const email = document.getElementById('email');
+      const sexo = document.querySelector('input[type=radio]:checked');
+      const cotizacion = document.getElementById('cotizacion') || null;
+      const rol_usuario = document.getElementById('rol_usuario');
+      const mensaje = document.getElementById("mensaje");
+      const data = {
+        'name': nombre.value,
+        'email': email.value,
+        'sexo': sexo.value,
+        'rol_usuario': rol_usuario.value,
+        'mensaje': mensaje.value
+      }; // Creamos un objecto con todos los elementos de nuestro formulario.
+      saveContactForm(data); // Enviamos la información obtenida por el usuario a la función que se encargara de guardar la información en Firebase
+      form.reset(); // borramos todos los campos. 
+    }
+
+  function saveContactForm(data) {
+    firebase.database().ref('contact').push(data) // Hacemos referencia al método database de el SDK y hacemos referencia el nombre del objeto que contendrá nuestros registros y empujamos los nuevos envios de datos
+      .then(function(){
+        alert('mensaje guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
+      })
+      .catch(function(){
+        alert('mensaje No guardado'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
+      });
+  };
