74e6144d2180b31ba528990e674f0e1b6f2593e3
eespitia16 <eespitiapicon@gmail.com>
2018-09-19 22:23:17 -0500

    conexiones de paginas

--- js/main.js
+++ js/main.js
@@ -1,77 +1,121 @@
-
-function registrar(){
-    var nombre, correo, usuario, clave, telefono;
-    nombre = document.getElementById('nombre').value;
-    apellidos = document.getElementById('apellidos').value;
-    correo = document.getElementById('correo').value;
-    usuario = document.getElementById('usuario').value;
-    clave = document.getElementById('clave').value;
-    telefono = document.getElementById('telefono').value;
-
-    if (nombre ===){
-      alert("El campo nombre esta vacío");
-    }
+  
+    var config = {
+      apiKey: "AIzaSyCCsQr6sJsoRK1ag1BTp685CerTcf5SYds",
+      authDomain: "hotel-daeca.firebaseapp.com",
+      databaseURL: "https://hotel-daeca.firebaseio.com",
+      projectId: "hotel-daeca",
+      storageBucket: "hotel-daeca.appspot.com",
+      messagingSenderId: "829762605681"
+    };
+    firebase.initializeApp(config);
     
-
+    const $btnSaveRegister = document.querySelector('#save-register')
+     $btnSaveRegister.addEventListener('click', sun())
     
-    firebase.auth().createUserWithEmailAndPassword(nombre, apellidos, correo, clave, telefono,expresion)
-    .catch(function(error) {
+  function registrar(){
+     
+      var email = document.getElementById('email').value;
+      var contrasena = document.getElementById('contrasena').value;
+  
+      firebase.auth().createUserWithEmailAndPassword(email, contrasena)
+      .then(function(){
+          verificar()
+      })
+      .catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       console.log(errorCode);
       console.log(errorMessage);
-
       // ...
-    }
-  });
-
-
-    var instanceR = M.Dropdown.getInstance(elemR);
-
-
-    document.addEventListener('DOMContentLoaded', function() {
-      var elemsR = document.querySelectorAll('.dropdown-trigger');
-      var instancesR = M.Dropdown.init(elemsR, options);
-      instanceR.open();
-
-    });
-
-
-
-    const form = document.getElementById('contactForm'); // Obtenemos la referencia al formulario
-
-    if(form){ // Si existe nuestro elemento en memoria este se quedara escuchando al evento submit del formulario
-      form.addEventListener('submit', contactForm); // Al momento de enviar el formulario, ejecuta la función "contactform"
-    }
-
-    function contactForm(event) {
-      event.preventDefault(); 
-    }
-    // Prevenimos el comportamiento por defecto de un formulario (Enviar por URL los parametros)
-      const nombre = document.getElementById('nombre'); // Obtenemos la referencia a cada uno de nuestros elementos del formulario
-      const email = document.getElementById('email');
-      const sexo = document.querySelector('input[type=radio]:checked');
-      const cotizacion = document.getElementById('cotizacion') || null;
-      const rol_usuario = document.getElementById('rol_usuario');
-      const mensaje = document.getElementById("mensaje");
-      const data = {
-        'name': nombre.value,
-        'email': email.value,
-        'sexo': sexo.value,
-        'rol_usuario': rol_usuario.value,
-        'mensaje': mensaje.value
-      };
-
-  function saveContactForm(data) {
-    firebase.database().ref('contact').push(data) // Hacemos referencia al método database de el SDK y hacemos referencia el nombre del objeto que contendrá nuestros registros y empujamos los nuevos envios de datos
+      });  
+  }
+  
+  function ingreso(){
+      var email2 = document.getElementById('email2').value;
+      var contrasena2 = document.getElementById('contrasena2').value;
+  
+      firebase.auth().signInWithEmailAndPassword(email2, contrasena2)
+      .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode);
+          console.log(errorMessage);
+          // ...
+        });
+  }
+  
+  function observador(){
+      firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+              console.log('usuario activo')
+              aparece(user);
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+  
+            console.log('************');
+            console.log(user.emailVerified)
+            console.log('************'); 
+  
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // ...
+          } else {
+            // User is signed out.
+            console.log('usuario no activo')
+            // ...
+          }
+        });
+  }
+  observador();
+  
+  function aparece(user){
+      var user = user;
+      var contenido = document.getElementById('contenido');
+      if(user.emailVerified){
+          contenido.innerHTML = `
+          <h2>Bienvenido.</h2>
+          <button onclick="cerrar()">cerrar sesion</button>
+          `;  
+      }
+  }
+  
+  function cerrar(){
+      firebase.auth().signOut()
       .then(function(){
-        alert('mensaje guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
+          console.log('saliendo.....')
       })
-      .catch(function(){
-        alert('mensaje No guardado'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
-      })};
-      document.addEventListener('DOMContentLoaded', function() {
-        var elemsR = document.querySelectorAll('select');
-        var instancesR = M.FormSelect.init(elemsR, options);
+      .catch(function(error){
+          console.log(error)
+      })
+  }
+  
+  function verificar(){
+      var user = firebase.auth().currentUser;
+      user.sendEmailVerification().then(function() {
+      // Email sent.
+      console.log('enviando correo...')
+      }).catch(function(error) {
+      // An error happened.
+      console.log('error');
       });
+  }
+  //registro administrador y recepcionista
+  
+function registrar(){
+  var nombre, correo, usuario, clave, telefono;
+  nombre = document.getElementById('nombre').value;
+  apellidos = document.getElementById('apellidos').value;
+  correo = document.getElementById('correo').value;
+  usuario = document.getElementById('usuario').value;
+  clave = document.getElementById('clave').value;
+  telefono = document.getElementById('telefono').value;
+
+  if (nombre ===){
+    alert("El campo nombre esta vacío");
+  }
