e1da53684cd8a521605d8209d20c070244afcddf
Aprendiz <Aprendiz@SGLDFPCAT7F571.FormacionSena.RED>
2018-09-28 12:00:48 -0500

    Habitaciones_iconos

--- js/habitaciones.js
+++ js/habitaciones.js
@@ -1,16 +1,26 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elemslider = document.querySelectorAll('.slider');
+    var arrowright = document.querySelector('.arrowright')
+    var arrowleft = document.querySelector('.arrowleft')
     var instanceslider = M.Slider.init(elemslider,{
-        indicators:false,
+        indicators:true,
         height:500,
         interval:1500
     });
     
-
+    arrowright.addEventListener('click', () => {
+      var instances = M.Slider.getInstance(elemslider[0])
+      instances.next()
+    })
+      arrowleft.addEventListener('click', () => {
+      var instances = M.Slider.getInstance(elemslider[0])
+      instances.prev() 
+      })
+      
 
   });
 
-
+  
   document.addEventListener('DOMContentLoaded', function() {
     var elemshora = document.querySelectorAll('.datepicker');
     var instancehora = M.Datepicker.init(elemshora);
