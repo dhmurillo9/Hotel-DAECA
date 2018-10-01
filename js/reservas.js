dabf834aa6d13c322007ca9abae1d3cadac3f310
eespitia16 <eespitiapicon@gmail.com>
2018-09-30 21:39:07 -0500

    barra de navegacion

--- js/reservas.js
+++ js/reservas.js
@@ -2,6 +2,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elemsnav = document.querySelectorAll('.sidenav');
     var instancesnav = M.Sidenav.init(elemsnav);
+    
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elemslider = document.querySelectorAll('.slider');
+    var arrowright = document.querySelector('.arrowright')
+    var arrowleft = document.querySelector('.arrowleft')
+    var instanceslider = M.Slider.init(elemslider,{
+        indicators:true,
+        height:500,
+        interval:1500
+    });
+    
+    arrowright.addEventListener('click', () => {
+      var instances = M.Slider.getInstance(elemslider[0])
+      instances.next()
+    })
+      arrowleft.addEventListener('click', () => {
+      var instances = M.Slider.getInstance(elemslider[0])
+      instances.prev() 
+      })
+      
+
+  });
+  
 
 
