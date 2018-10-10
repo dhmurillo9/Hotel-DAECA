12615d2a13358e3c3af034f74e38c6d98c875e8b
adriana0198 <adrianalucia110198@gmail.com>
2018-10-10 06:46:52 -0500

    arreglo login

--- js/login.js
+++ js/login.js
@@ -3,7 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
   var instancesnav = M.Sidenav.init(elemsnav);
   
 });
-
+  $('.toggle').click(function(){
+    $('.formulario').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+    }, "slow");
+});
 
 var config = {
   apiKey: "AIzaSyB3G1sNiRiMGXR48wR0FAjlF_zaIsMA6c8",
@@ -12,25 +19,53 @@ var config = {
   projectId: "hotel-daeca-244ad",
   storageBucket: "hotel-daeca-244ad.appspot.com",
   messagingSenderId: "957899814726"
-};
+  };
   firebase.initializeApp(config);
 
-const $formRegister = document.querySelector('.form-ingresar')
-var firebaseDB = firebase.database()
-var refUsers = firebase.database().ref('users')
-var user
+  const $formRegister = document.querySelector('.form-register')
+  var firebaseDB = firebase.database()
+  var refUsers = firebase.database().ref('users')
+  var user
+  
+  $formRegister.addEventListener('submit', async function (event) {
+      event.preventDefault()
+      const dataForm = new FormData($formRegister)
+      user = await firebase.auth().createUserWithEmailAndPassword(dataForm.get('email'), dataForm.get('pass'))
+          .catch(function (error) {
+              var errorCode = error.code;
+              var errorMessage = error.message;
+          })
+      let newUser = {
+          usuario: dataForm.get('usuario'),
+          cedula: dataForm.get('cedula'),
+          telefono: dataForm.get('telefono'),
+          correo: dataForm.get('email'),
+          rol: 'cliente'
+      }
+      firebase.database().ref(`users/${user.user.uid}`).set(newUser).then(() => {
+          swal("Todo bien!", "ya te as registrado!", "success")
+          $formRegister.reset()
+      }).catch((e) => {
+          console.log('isiisis')
+      })
+  })
+  
 
-function validar(){
-  var usuario = document.getElementById("usuario").value;
-  var contraseña = document.getElementById("contraseña").value;
+
+  
+  function ingreso(){
+    var email2 = document.getElementById('email2').value;
+    var pass2 = document.getElementById('pass2').value;
+
+    firebase.auth().signInWithEmailAndPassword(email2, pass2)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
 }
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-  } else {
-    // No user is signed in.
-  }
-});
 
 
--- js/main.js
+++ js/main.js
@@ -15,107 +15,7 @@ var config = {
   };
     firebase.initializeApp(config);
   
-    const $btnSaveRegister = document.querySelector('#save-register')
-    if($btnSaveRegister) $btnSaveRegister.addEventListener('click', sun())
-    
-    var btncerrarsesion = document.querySelector('.cerrarsesion');
-    btncerrarsesion.addEventListener('click', ()=>{
-        cerrarsesion();
-    })
 
-  function registrar(){
-     
-      var email = document.getElementById('email').value;
-      var contrasena = document.getElementById('contrasena').value;
-  
-      firebase.auth().createUserWithEmailAndPassword(email, contrasena)
-      .then(function(){
-          verificar()
-      })
-      .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode);
-      console.log(errorMessage);
-      // ...
-      });  
-  }
-  
-  function ingreso(){
-      var email2 = document.getElementById('email2').value;
-      var contrasena2 = document.getElementById('contrasena2').value;
-  
-      firebase.auth().signInWithEmailAndPassword(email2, contrasena2)
-      .catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.log(errorCode);
-          console.log(errorMessage);
-          // ...
-        });
-  }
-  
-//   function observador(){
-//       firebase.auth().onAuthStateChanged(function(user) {
-//           if (user) {
-//               console.log('usuario activo')
-//               aparece(user);
-//             // User is signed in.
-//             var displayName = user.displayName;
-//             var email = user.email;
-  
-//             console.log('************');
-//             console.log(user.emailVerified)
-//             console.log('************'); 
-  
-//             var emailVerified = user.emailVerified;
-//             var photoURL = user.photoURL;
-//             var isAnonymous = user.isAnonymous;
-//             var uid = user.uid;
-//             var providerData = user.providerData;
-//             // ...
-//           } else {
-//             // User is signed out.
-//             console.log('usuario no activo')
-//             // ...
-//           }
-//         });
-//   }
-//   observador();
-  
-  function aparece(user){
-      var user = user;
-      var contenido = document.getElementById('contenido');
-      if(user.emailVerified){
-          contenido.innerHTML = `
-          <h2>Bienvenido.</h2>
-          <button onclick="cerrar()">cerrar sesion</button>
-          `;  
-      }
-  }
-  
-  function cerrar(){
-      firebase.auth().signOut()
-      .then(function(){
-          console.log('saliendo.....')
-      })
-      .catch(function(error){
-          console.log(error)
-      })
-  }
-  
-  function verificar(){
-      var user = firebase.auth().currentUser;
-      user.sendEmailVerification().then(function() {
-      // Email sent.
-      console.log('enviando correo...')
-      }).catch(function(error) {
-      // An error happened.
-      console.log('error');
-      });
-  }
   //registro administrador y recepcionista
   
 function registrar(){
@@ -131,8 +31,6 @@ function registrar(){
     alert("El campo nombre esta vacío");
   }
 }
-<<<<<<< HEAD
-=======
 
   //slider de página de reservas
   document.addEventListener('DOMContentLoaded', function() {
@@ -159,4 +57,4 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.datepicker');
     var instances = M.Datepicker.init(elems);
   });
->>>>>>> ee9a8b5a5ddef7b7c115cf9f80ae24b33379993f
+
